e09bf50d828e8736d04a8f05cc7db1ba1aa1b9d7
iradaglly <iradagulaliyeva18@mail.ru>
2023-03-06 01:10:04 +0400

    changed array size

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace resize_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            Console.WriteLine("Please enter the size of the array");
+            int sizeArray = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[sizeArray];
+            int increment = 0;
+            do
+            {
+                Console.WriteLine("Enter element ");
+                numbers[increment] = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Do you want to continue? ");
+
+                string answer = Console.ReadLine();
+
+                if (answer == "y" || answer == "Y")
+                {
+                    increment++;
+                    Array.Resize(ref numbers, numbers.Length + 1);
+                }
+                else
+                {
+                    flag = false;
+                    Console.WriteLine("...exiting");
+                    break;
+                }
+
+
+            } while (flag);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+        }
+    }
+}
